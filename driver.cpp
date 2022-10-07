97fb8836d56caf66539d52b3d794023d0a03fcbb
Chris Tralie <chris.tralie@gmail.com>
2022-10-07 13:22:22 -0400

    Setting up code for today

--- driver.cpp
+++ driver.cpp
@@ -2,20 +2,30 @@
 #include "person.h"
 using namespace std;
 
-int main() {
-    SoccerPlayer hannah("Hannah", "J", 60, 2026, true, "defense");
-    Person chris("Chris", "Tralie", 33, 2011);
-    Student tre("Tre", "D", 174, 2025, "cs");
-
-    int N = 3;
-    Person** people = new Person*[N];
-    people[0] = &hannah;
-    people[1] = &chris;
-    people[2] = &tre;
-
+/**
+ * @brief Print out the array of people
+ * 
+ * @param people Array of Person references
+ * @param N Number of people in the array
+ */
+void printPeople(Person** people, int N) {
     for (int i = 0; i < N; i++) {
         people[i]->printInfo();
         cout << "\n\n";
     }
+}
+
+int main() {
+    int N = 5;
+    Person** people = new Person*[N];
+    people[0] = new SoccerPlayer("Hannah", "J", 60, 2026, true, "defense");
+    people[1] = new Student("Tre", "D", 174, 2025, "cs");
+    people[2] = new Person("Chris", "Tralie", 33, 2011);
+    people[3] = new Student("Albert", "Einstein", 143, 1900, "teaching");
+    people[4] = new Athlete("Usain", "Bolt", 36, 2008, "track", true);
+
+    printPeople(people, N);
+
+    // TODO: There is a memory leak
     delete[] people;
 }
